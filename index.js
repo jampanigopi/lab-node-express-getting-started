832424637213d2937976ca8d77fc9aabab33a6c2
jampanigopi <gopijampani84@gmail.com>
2022-07-29 11:06:12 +0530

    done

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+const port = 5000;
+const eport = 2000;
+
+http.createServer((request, response) => {
+    response.write('This is http server');
+    response.end();
+}).listen(port, () =>
+    console.log(`Http server started at http://localhost:${port}`)
+);
+
+app.get('/', (request, response) => {
+    response.send('This is express server');
+});
+app.listen(eport, () =>
+    console.log(`Express server started at http://localhost:${eport}`)
+);
